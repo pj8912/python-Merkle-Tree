646f08c5757b5f30d847fabbb8ed900b6bde75c8
pj8912 <jpintofun2b@gmail.com>
2022-07-03 14:43:21 +0530

    intial commit

--- Merkle.py
+++ Merkle.py
@@ -7,4 +7,16 @@ class Merkle:
         self.val = val
         self.left = None
         self.right = None
+   
+
+   def hash(self, val):
+       return hashlib.sha256(val.encode('utf-8')).hexdigest()
+   
+   def doubleHash(self, hashed_val):
+       return self.hash(hashed_val)
     
+
+    
+    
+   def getRootHash(self):
+       return self.rootHash
